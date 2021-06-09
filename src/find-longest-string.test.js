4a351fb9d3b059b58d83045f652fd206e48107dd
Yunas A <yatteyeh@gmail.com>
2021-06-09 11:53:26 +0300

    firstchange

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -8,4 +8,6 @@ test("findLongestString finds the longest string in an array", () => {
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["golden", "baller", "pallet", "pallets"])).toBe("golden");
+  expect(findLongestString(["basketball", "waferboard", "waferboards", "wafer"])).toBe("basketball");
 });
